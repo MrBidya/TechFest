2558213caa5b4870310355f3cc638b6b5e1c9e02
ViktorBarzin <vbarzin@gmail.com>
2017-05-26 01:23:11 +0300

    refactored quadradit equation solver

--- core/algebra-test.py
+++ core/algebra-test.py
@@ -3,44 +3,25 @@ import unittest
 from string import ascii_letters
 from hypothesis import given
 from hypothesis.strategies import text, integers, just, sampled_from
+from algebra import QuadraticEquation
 
 
-class AlgebraTests(unittest.TestCase):
+# Todo: write algebra tests
+class QuadraticEquationTests(unittest.TestCase):
     def setUp(self):
        self.algebra = a.Algebra()
 
-    @given(integers(min_value=2, max_value=30))
-    def test_sqrt_exact_number(self, inp):
-        self.assertEqual(inp, self.algebra.sqrt(inp * inp))
-
-    @given(integers(max_value=-1))
-    def test_sqrt_invalid_number(self, inp):
-        self.assertEqual('Invalid number', self.algebra.sqrt(inp))
-
-    @given(text(alphabet=ascii_letters))
-    def test_sqrt_not_a_number(self, text):
-        self.assertEqual('Invalid number', self.algebra.sqrt(text))
-
-    @given(integers(min_value=2, max_value=60))
-    def test_sqrt_root_of_non_squarable(self, inp):
-        res = self.algebra.sqrt(inp * inp + 1)
-        assert self.algebra.SQRT_SIGN in res
-
     def test_quadratic_equation_2_real_roots(self):
-        self.assertEqual('x1 = (6 + 2.0√2) / 5.0\nx2 = (6 - 2.0√2) / 5.0', self.algebra.quadratic_equation('25 * x ^ 2 - 60 * x + 28'))
+        self.assertEqual('(2*sqrt(2)/5 + 6/5, -2*sqrt(2)/5 + 6/5)', str(QuadraticEquation.from_str('25 * x ^ 2 - 60 * x + 28').solve()))
 
     def test_quadratic_equation_2_natural_roots(self):
-        self.assertEqual('x1 = 13.0\nx2 = 2.0', self.algebra.quadratic_equation('x^2 - 15 * x + 26'))
+        self.assertEqual((13, 2), QuadraticEquation.from_str('x^2 - 15 * x + 26').solve())
 
     @given(integers(min_value=1), integers(min_value=3, max_value=4), integers(min_value=17))
     def test_quadratic_equation_no_roots(self, a, b, c):
-        assert self.algebra.quadratic_equation('{}*x^2 + {}*x + {}'.format(a, b ,c)) == 'No real roots'
+        eq = QuadraticEquation.from_str('{}*x^2 + {}*x + {}'.format(a, b ,c))
+        self.assertRaises(ValueError,eq.solve)
 
 if __name__ == '__main__':
     unittest.main()
-    pass
-
-
-
-# test_add_one()
 
--- core/algebra.py
+++ core/algebra.py
@@ -1,6 +1,7 @@
 import re
 import math
 import sys
+import sympy
 from collections import defaultdict
 from _fractions import simplify_fraction as simplify_frac
 from meta import AlgebraMeta
@@ -9,9 +10,8 @@ from meta import AlgebraMeta
 class Algebra(metaclass=AlgebraMeta):
     def __init__(self):
         self.SQRT_SIGN = '√'
-        self.INVALID_QUADRATIC_EQUATION = 'Invalid quadratic equation!'
 
-    def __get_primes(self, n):
+    def get_primes(self, n):
         primes = []
         count = 2
         while count < n:
@@ -25,13 +25,13 @@ class Algebra(metaclass=AlgebraMeta):
             count += 1
         return primes
 
-    def __get_prime_delimeters(self, n):
-        primes_to_n = self.__get_primes(n)
+    def get_prime_delimeters(self, n):
+        primes_to_n = self.get_primes(n)
         prime_delimeters = [x for x in primes_to_n if n % x == 0]
         return prime_delimeters
 
-    def __decompose_number(self, number):
-        primes = self.__get_primes(number)
+    def decompose_number(self, number):
+        primes = self.get_primes(number)
         decomposed = []
         index = 0
         while number not in primes and index < len(primes):
@@ -44,7 +44,7 @@ class Algebra(metaclass=AlgebraMeta):
             decomposed.remove(1)
         return decomposed
 
-    def __create_histogram(self, items):
+    def create_histogram(self, items):
         histogram = defaultdict(int)
         for i in items:
             histogram[i] += 1
@@ -55,11 +55,11 @@ class Algebra(metaclass=AlgebraMeta):
         return [x for x in range(1, n + 1) if n % x == 0]
 
     def get_common_divisors(self, divisors_string):
-        # this method is to prepare inner call so that user can call this
+        # This method prepares inner call so that user can call this
         # method directly
 
         # Numbers can be split by anything but a number e.g. : 40 $2 should
-        # parse to 40, 2
+        # parse to (40, 2)
         divisors = re.findall('[0-9]+', divisors_string)
 
         if len(divisors) != 2:
@@ -71,105 +71,6 @@ class Algebra(metaclass=AlgebraMeta):
         denominator_divisors = self.get_divisors(b)
         return sorted(list(set(nominator_divisors) & set(denominator_divisors)))
 
-    def sqrt(self, number, calculate=False):
-        if calculate:
-            return math.sqrt(number)
-        try:
-            number = int(number)
-            if number < 0:
-                raise
-        except:
-            return 'Invalid number'
-        compounds = self.__decompose_number(number)
-        histogram = dict(self.__create_histogram(compounds))
-
-        coef = 1
-        under_root = 0
-        for k, v in histogram.items():
-            if v == 1:
-                under_root *= k
-            if v % 2 == 1:
-                under_root += k
-                v -= 1
-
-            while v % 2 == 0 and v > 0:
-                coef *= k
-                v -= 2
-        #print(histogram)
-        #print(coef, under_root)
-
-        # prepare answer for printing
-        if coef == 1:
-            return self.SQRT_SIGN + str(number)
-        if under_root == 1 or under_root == 0:
-            return coef
-        return str(coef) + self.SQRT_SIGN + str(under_root)
-
-    def __quadratic_equation(self, equation, var):
-        split_equation = [x.replace(' ','') for x in equation.split(var)]
-        try:
-            a = int(split_equation[0][:-1])
-            if a == 0:
-                return self.INVALID_QUADRATIC_EQUATION
-        except ValueError:
-            a = 1
-        try:
-            b = int(split_equation[1].split('^2')[1][:-1])
-            if b == 0:
-                return self.INVALID_QUADRATIC_EQUATION
-        except ValueError:
-            b = 1
-        try:
-            c = int(split_equation[-1])
-        except ValueError:
-            c = 1
-
-        d = b ** 2 - 4 * a * c
-
-       # print(a,b,c,d)
-        if d < 0:
-            return 'No real roots'
-        elif d == 0:
-            # 1 root
-            x = -b  / (2 * a)
-            return 'x1 = x2 = {0}'.format(x)
-        # 2 roots
-        sqrt_d = self.sqrt(d)
-        x1 = '({} + {}) / {}'.format(-b, sqrt_d, 2 * a)
-        x2 = '({} - {}) / {}'.format(-b, sqrt_d, 2 * a)
-       # Check if can be calculated if not - simplify result
-        if self.SQRT_SIGN not in x1:
-           x1 = eval(str(x1))
-        else:
-            x1 = self.__simplify_quadratic_result(sqrt_d, a, b, '+')
-        if self.SQRT_SIGN not in x2:
-            x2 = eval(str(x2))
-        else:
-            x2 = self.__simplify_quadratic_result(sqrt_d, a, b, '-')
-        return 'x1 = {}\nx2 = {}'.format(x1, x2)
-
-    # Having (4 + 4*sqrt(2)) / 2 will divide all by 2 -> 2 + 2*Sqrt(2)
-    # The format sign is to format the result with the correct sign
-    def __simplify_quadratic_result(self, sqrt_d, a, b, format_sign):
-            split_root = sqrt_d.split(self.SQRT_SIGN)
-            try:
-                root_coef = int(split_root[0])
-            except ValueError:
-                root_coef = 1
-            b_2a_divisors = self.__get_common_divisors(-b, 2 * a)
-            sqrt_d_2a_divisors = self.__get_common_divisors(root_coef, 2 * a)
-            common_divisors = [x for x in b_2a_divisors if x in sqrt_d_2a_divisors]
-            if len(common_divisors) > 0:
-                b /= max(common_divisors)
-                root_coef /= max(common_divisors)
-                a /= max(common_divisors)
-            return ('({} {} {}{}{}) / {}'.format(int(-b), format_sign, root_coef, self.SQRT_SIGN, split_root[1], 2 * a)).replace('1.0', '')
-
-    def quadratic_equation(self, equation):
-        for letter in [chr(x) for x in range(97, 123)]:
-            if letter in equation:
-                return self.__quadratic_equation(equation, letter)
-        return 'Invalid variable'
 
     def simplify_fraction(self, fraction):
         fraction = [int(x) for x in fraction.split('/')]
@@ -178,30 +79,90 @@ class Algebra(metaclass=AlgebraMeta):
         return simplify_frac((fraction))
 
 
-def main():
-    algebra = Algebra()
-    # choose_math_func = input('Choose math func (sqrt/quad):')
+class QuadraticEquation:
+    def __init__(self, a=1, b=1, c=1, var='x', res1=None, res2=None, string=None):
+        self.SQRT_SIGN = '√'
+        self.INVALID_QUADRATIC_EQUATION = 'Invalid quadratic equation!'
 
-    options = {x: algebra.methods[x] for x in range(len(algebra.methods))}
+        self.a = a
+        self.b = b
+        self.c = c
+        self.var = var
+        self.res1 = res1
+        self.res2 = res2
+        if string is not None:
+            self.string = string
+        else:
+            self.string = '{}*{}^2 {}*{} {}'.format(self.a, self.var, self.b, self.var, self.c)
 
-    print_str = ''
-    for key in options.keys():
-        print_str += '{}) {}\n'.format(key, options[key])
-    method_choice = int(input(print_str + '\n' + '>>>'))
-    # import ipdb; ipdb.set_trace()# BREAKPOINT)
-    inp = input('Enter your problem:')
-    # import ipdb; ipdb.set_trace()# BREAKPOINT)
+    def __repr__(self):
+        return self.string
 
-    # print(options[method_choice](inp))
+    def __str__(self):
+        return self.__repr__()
 
-    # print(eval('algebra.{}(str({}))'.format(options[method_choice], inp)))
-    # print(inp)
-    # print(eval('algebra.{}('.format(options[method_choice])+ str(inp) + ')'))
-    # print(inp)
-    result = getattr(algebra, options[method_choice])(inp)
-    print(result)
-    # print(options[choose_math_func](inp))
+    def solve(self):
+        d = self.b ** 2 - 4 * self.a * self.c
+        if d < 0:
+            raise ValueError('No real roots')
+        self.res1 = (-self.b + sympy.sqrt(d)) / (2 * self.a)
+        self.res2 = (-self.b - sympy.sqrt(d)) / (2 * self.a)
+
+        return self.res1, self.res2
 
 
-if __name__ == '__main__':
-    main()
+    @staticmethod
+    def __extract_var(input_str):
+        for letter in [chr(x) for x in range(97, 123)]:
+            # lower lowers all letters
+            if letter in input_str.lower():
+                return letter
+        raise ValueError('Cannot parse equation variable')
+
+    @staticmethod
+    def __get_quad_coeffs(input_str):
+        var = QuadraticEquation.__extract_var(input_str)
+        try:
+            a = int(input_str[:input_str.index(var)].replace('*', ''))
+        except ValueError:
+            if input_str[0] == '-':
+                a = -1
+            else:
+                a = 1
+
+        try:
+            b = input_str.split(var)[1].split('^2')[1].replace('*', '')
+            b = int(b)
+        except ValueError:
+            if b == '-':
+                b = -1
+            elif b == '+':
+                b = 1
+        # This takes the last number.
+        # IMPORTANT:
+        # The input string MUST be in valid format or else it will break
+        c = int(re.findall(r'[-+]*\d+', input_str)[-1])
+        return a, b, c
+
+
+    @staticmethod
+    def from_str(input_str, var=None):
+        # Extract var if not passed
+        input_str = input_str.replace(' ','')
+        if var is None:
+            var = QuadraticEquation.__extract_var(input_str)
+        coefs = re.findall(r'-?[0-9]+', input_str)
+        if len(coefs) != 4:
+            a, b, c = QuadraticEquation.__get_quad_coeffs(input_str)
+#            raise ValueError('Not enough coefficients passed. Must be 4, instead got {}'.format(len(coefs)))
+        else:
+        # Coefs contains the following:
+            # index 0 is a
+            a = int(coefs[0])
+            # index 1 is the power of x which must be always 2
+            # index 2 is b
+            b = int(coefs[2])
+            # index 3 is c
+            c = int(coefs[3])
+
+        return QuadraticEquation(a=a, b=b, c=c, var=var, string=input_str.replace(' ',''))
