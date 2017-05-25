9acf1a5ad3403f7d7a15d3205810dcb3e43a93c0
ViktorBarzin <vbarzin@gmail.com>
2017-05-25 15:55:05 +0300

    moving stuff around

---
+++ api/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import authentication, permissions
+
+# Create your views here.
+
+class IndexView(APIView):
+    def get(self, request):
+        return Response(data={'fooasdasd':'bar'})
+
